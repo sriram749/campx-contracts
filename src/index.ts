08b56b8b2193d8d2d1ddf7b0f9eed0e1020e190b
CampX Dev <dev@campx.com>
2026-02-26 15:35:39 +0530

    feat: add barrel exports (index.ts) for all contract modules

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// LMS contracts
+export * from './lms/assignments.dto';
+export * from './lms/quizzes.dto';
+
+// Payments contracts
+export * from './payments/fee-types.dto';
+
+// Academic contracts
+export * from './academic/courses.dto';
